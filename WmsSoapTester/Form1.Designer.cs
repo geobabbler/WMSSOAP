ce1a6ab7a2dace769bfab3554e2fa9cd1c7d0c70
Bill Dollins <bill@geomusings.com>
2016-05-16 07:07:30 -0400

    updated test client

--- WmsSoapTester/Form1.Designer.cs
+++ WmsSoapTester/Form1.Designer.cs
@@ -28,50 +28,244 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lstLayers = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMinx = new System.Windows.Forms.TextBox();
+            this.txtMaxx = new System.Windows.Forms.TextBox();
+            this.txtMaxy = new System.Windows.Forms.TextBox();
+            this.txtMiny = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lstFormat = new System.Windows.Forms.ListBox();
+            this.cboSrs = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(650, 276);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.cboSrs);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.lstFormat);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.txtMiny);
+            this.tabPage1.Controls.Add(this.txtMaxy);
+            this.tabPage1.Controls.Add(this.txtMaxx);
+            this.tabPage1.Controls.Add(this.txtMinx);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.lstLayers);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(642, 250);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Capabilities";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SRS";
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(370, 12);
+            this.button1.Location = new System.Drawing.Point(545, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 47);
-            this.button1.TabIndex = 0;
+            this.button1.Size = new System.Drawing.Size(91, 22);
+            this.button1.TabIndex = 1;
             this.button1.Text = "Get Capabilities";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(642, 250);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Map";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(370, 65);
+            this.button2.Location = new System.Drawing.Point(549, 7);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(87, 40);
-            this.button2.TabIndex = 1;
+            this.button2.Size = new System.Drawing.Size(87, 21);
+            this.button2.TabIndex = 4;
             this.button2.Text = "Get Map";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(153, 7);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(337, 237);
-            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
+            // 
+            // lstLayers
+            // 
+            this.lstLayers.FormattingEnabled = true;
+            this.lstLayers.Location = new System.Drawing.Point(182, 22);
+            this.lstLayers.Name = "lstLayers";
+            this.lstLayers.Size = new System.Drawing.Size(150, 56);
+            this.lstLayers.Sorted = true;
+            this.lstLayers.TabIndex = 4;
+            this.lstLayers.SelectedValueChanged += new System.EventHandler(this.lstLayers_SelectedValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Layers";
+            // 
+            // txtMinx
+            // 
+            this.txtMinx.Location = new System.Drawing.Point(6, 128);
+            this.txtMinx.Name = "txtMinx";
+            this.txtMinx.Size = new System.Drawing.Size(126, 20);
+            this.txtMinx.TabIndex = 6;
+            // 
+            // txtMaxx
+            // 
+            this.txtMaxx.Location = new System.Drawing.Point(206, 128);
+            this.txtMaxx.Name = "txtMaxx";
+            this.txtMaxx.Size = new System.Drawing.Size(126, 20);
+            this.txtMaxx.TabIndex = 7;
+            // 
+            // txtMaxy
+            // 
+            this.txtMaxy.Location = new System.Drawing.Point(105, 102);
+            this.txtMaxy.Name = "txtMaxy";
+            this.txtMaxy.Size = new System.Drawing.Size(126, 20);
+            this.txtMaxy.TabIndex = 8;
+            // 
+            // txtMiny
+            // 
+            this.txtMiny.Location = new System.Drawing.Point(105, 154);
+            this.txtMiny.Name = "txtMiny";
+            this.txtMiny.Size = new System.Drawing.Size(126, 20);
+            this.txtMiny.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(146, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Max Y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(146, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Min Y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Min X";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(295, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Max X";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(358, 6);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Format";
+            // 
+            // lstFormat
+            // 
+            this.lstFormat.FormattingEnabled = true;
+            this.lstFormat.Items.AddRange(new object[] {
+            "image/gif",
+            "image/jpeg",
+            "image/png"});
+            this.lstFormat.Location = new System.Drawing.Point(361, 22);
+            this.lstFormat.Name = "lstFormat";
+            this.lstFormat.Size = new System.Drawing.Size(150, 56);
+            this.lstFormat.Sorted = true;
+            this.lstFormat.TabIndex = 14;
+            // 
+            // cboSrs
+            // 
+            this.cboSrs.FormattingEnabled = true;
+            this.cboSrs.Location = new System.Drawing.Point(6, 22);
+            this.cboSrs.Name = "cboSrs";
+            this.cboSrs.Size = new System.Drawing.Size(126, 21);
+            this.cboSrs.TabIndex = 16;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 261);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(665, 317);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WMS SOAP Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -79,9 +273,26 @@
 
         #endregion
 
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstLayers;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMiny;
+        private System.Windows.Forms.TextBox txtMaxy;
+        private System.Windows.Forms.TextBox txtMaxx;
+        private System.Windows.Forms.TextBox txtMinx;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox lstFormat;
+        private System.Windows.Forms.ComboBox cboSrs;
     }
 }
 
--- WmsSoapTester/Form1.cs
+++ WmsSoapTester/Form1.cs
@@ -23,22 +23,64 @@ namespace WmsSoapTester
         {
             WmsClient client = new WmsClient();
             var cap = client.GetCapabilities();
+            var crslist = cap.capabilityField.layerField.cRSField;
+            var lyrs = cap.capabilityField.layerField.layer1Field;
+            this.cboSrs.DataSource = crslist;
+            //lstSRS.DataSource = crslist;
+            //foreach(string crs in crslist)
+            //{
+            //    this.lstSRS.Items.Add(crs);
+            //}
+            //lstSRS.Items.Insert(0, "EPSG:4326");
+            lstLayers.DisplayMember = "nameField";
+            lstLayers.DataSource = lyrs;
             client.Close();
             
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string[] lyrs = new string[] { "sf:roads" };
-            string crs = "EPSG:26713";
-            //589434.8564686741,4914006.337837095,609527.2102150217,4928063.398014731
-            wmssoap.BoundingBox bbox = new BoundingBox() { minxField = 589434.8564686741, minyField = 4914006.337837095, maxxField = 609527.2102150217, maxyField = 4928063.398014731 };
+            List<string> lyrlist = new List<string>();
+            foreach(wmssoap.Layer itm in this.lstLayers.SelectedItems)
+            {
+                lyrlist.Add(itm.nameField);
+            }
+            //589434.8564686741,4914006.337837095,609527.2102150217,4928063.398014731&width=768&height=537&srs=EPSG:26713
+            string[] lyrs = lyrlist.ToArray();
+            string crs = this.cboSrs.Text;
+            //,609527.2102150217,4928063.398014731
+            wmssoap.BoundingBox bbox = new BoundingBox() { minxField = Convert.ToDouble(this.txtMinx.Text), minyField = Convert.ToDouble(this.txtMiny.Text), maxxField = Convert.ToDouble(this.txtMaxx.Text), maxyField = Convert.ToDouble(this.txtMaxy.Text) };
             WmsClient client = new WmsClient();
-            var stm = client.GetMap(lyrs, new string[0], bbox, 337, 237, crs, "image/png");
+            var stm = client.GetMap(lyrs, new string[0], bbox, 337, 237, crs, this.lstFormat.SelectedItem.ToString());
             var img = Image.FromStream(stm.BinaryPayload);
             this.pictureBox1.Image = img;
+            //this.pictureBox1.Refresh();
             client.Close();
 
+        }
+
+        private void lstLayers_SelectedValueChanged(object sender, EventArgs e)
+        {
+            wmssoap.Layer lyr = this.lstLayers.SelectedItem as wmssoap.Layer;
+            wmssoap.BoundingBox bbox = null;
+            foreach(wmssoap.BoundingBox box in lyr.boundingBoxField)
+            {
+                if (box.cRSField.ToLower() == lyr.cRSField[0].ToLower())
+                {
+                    bbox = box;
+                    break;
+                }
+            }
+            this.txtMaxx.Text = bbox.maxxField.ToString();
+            this.txtMaxy.Text = bbox.maxyField.ToString();
+            this.txtMinx.Text = bbox.minxField.ToString();
+            this.txtMiny.Text = bbox.minyField.ToString();
+            this.cboSrs.Text = lyr.cRSField[0];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.lstFormat.SelectedIndex = 2;
         }
     }
 }
