6a25406f08ddf45d28a49ec421b9515e86b060e9
Bill Dollins <bill@geomusings.com>
2016-07-14 17:10:03 -0400

    added support for POST calls to WMS

--- WmsSoap/IWms.cs
+++ WmsSoap/IWms.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
@@ -11,6 +12,7 @@ using OGC.WMS.SOAP.v1_3_0;
 namespace OGC.WMS.SOAP
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceKnownType("GetKnownTypes", typeof(Helper))]
     [ServiceContract (Namespace = "http://schemas.opengis.net/wms/soap")]
     public interface IWms
     {
@@ -24,35 +26,31 @@ namespace OGC.WMS.SOAP
         [OperationContract]
         BinaryResponse GetMap(string[] layers, string[] styles, BoundingBox bounds, int width, int height, string crs, string format);
 
-        //[OperationContract]
-        //string GetData(int value);
+        [OperationContract]
+        BinaryResponse PostMap(OGC.WMS.SOAP.SLD.GetMapType getMapSld);
 
-        //[OperationContract]
-        //CompositeType GetDataUsingDataContract(CompositeType composite);
+     }
 
-        // TODO: Add your service operations here
+    static class Helper
+    {
+        public static IEnumerable<Type> GetKnownTypes(ICustomAttributeProvider provider)
+        {
+            System.Collections.Generic.List<System.Type> knownTypes =
+                new System.Collections.Generic.List<System.Type>();
+            // Add any types to include here.
+            knownTypes.Add(typeof(OGC.WMS.SOAP.SLD.NamedLayer));
+            knownTypes.Add(typeof(OGC.WMS.SOAP.SLD.UserLayer));
+            knownTypes.Add(typeof(OGC.WMS.SOAP.SLD.NamedStyle));
+            knownTypes.Add(typeof(OGC.WMS.SOAP.SLD.EnvelopeType));
+            knownTypes.Add(typeof(OGC.WMS.SOAP.SLD.ItemChoiceType));
+            knownTypes.Add(typeof(OGC.WMS.SOAP.SLD.VersionType));
+            knownTypes.Add(typeof(OGC.WMS.SOAP.SLD.OutputType));
+            knownTypes.Add(typeof(OGC.WMS.SOAP.SLD.CoordType));
+            return knownTypes;
+        }
     }
 
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
 
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
 }
--- WmsSoap/Wms130.svc.cs
+++ WmsSoap/Wms130.svc.cs
@@ -178,5 +178,94 @@ namespace OGC.WMS.SOAP
             //WebOperationContext.Current.OutgoingResponse.ContentType = format;
             //return ms;
         }
+        
+        public BinaryResponse PostMap (OGC.WMS.SOAP.SLD.GetMapType getMapSld)
+        {
+            BinaryResponse retval = new BinaryResponse();
+            HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(System.Configuration.ConfigurationManager.AppSettings["baseUrl"]);
+            Stream responseStream = null;
+            MemoryStream ms;
+            var postData = SerializeSldObject(getMapSld);
+            var data = Encoding.UTF8.GetBytes(postData);
+            httpRequest.Method = "POST";
+            httpRequest.ContentType = "text/xml";
+            httpRequest.ContentLength = data.Length;
+            using (var stream = httpRequest.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+            using (var httpResponse = (HttpWebResponse)httpRequest.GetResponse())
+            {
+                using (responseStream = httpResponse.GetResponseStream())
+                {
+                    ms = new MemoryStream();
+
+                    byte[] buffer = new byte[1024];
+                    int byteCount;
+                    do
+                    {
+                        byteCount = responseStream.Read(buffer, 0, buffer.Length);
+                        ms.Write(buffer, 0, byteCount);
+                    } while (byteCount > 0);
+                    ms.Position = 0;
+                    retval.ContentType = getMapSld.Output.Format;
+                    retval.BinaryPayload = ms;
+                    return retval;
+                }
+
+
+                //using (var streamRdr = new StreamReader(responseStream))
+                //{
+                //    var response = streamRdr.ReadToEnd();
+
+                //    httpResponse.Close();
+
+                //    return LoadFromString(response);
+                //}
+            }
+            //return true;
+        }
+
+        private string SerializeSldObject(OGC.WMS.SOAP.SLD.GetMapType getMapSld)
+        {
+            string retval = "";
+            try
+            {
+                XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+                ns.Add("ogc", "http://www.opengis.net/ows");
+                ns.Add("gml", "http://www.opengis.net/gml");
+                System.Xml.Serialization.XmlSerializer x = new System.Xml.Serialization.XmlSerializer(getMapSld.GetType());
+                var stringwriter = new Utf8StringWriter(); // new System.IO.StringWriter();
+                //stringwriter. = Encoding.UTF8;
+                x.Serialize(stringwriter, getMapSld, ns);
+                retval = stringwriter.ToString();
+                Console.WriteLine(retval);
+            }
+            catch(System.Exception ex)
+            {
+                //XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+                //ns.Add("ogc", "http://www.opengis.net/ows");
+                //ns.Add("gml", "http://www.opengis.net/gml");
+                //System.Xml.Serialization.XmlSerializer x = new System.Xml.Serialization.XmlSerializer(getMapSld.GetType());
+                //var stringwriter = new System.IO.StringWriter();
+                //x.Serialize(stringwriter, new OGC.WMS.SOAP.SLD.GetMapType(), ns);
+                //retval = stringwriter.ToString();
+            }
+            return retval;
+        }
+
+        //public OGC.WMS.SOAP.SLD.NamedLayer GetNamedLayer()
+        //{
+        //    return new SLD.NamedLayer();
+        //}
+    }
+
+    public class Utf8StringWriter : StringWriter
+    {
+        // Use UTF8 encoding but write no BOM to the wire
+        public override Encoding Encoding
+        {
+            get { return new UTF8Encoding(false); } // in real code I'll cache this encoding.
+        }
     }
 }
